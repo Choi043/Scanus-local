111f7ece63556d54dfd69c5552255c9680c62c5c
Choi043 <rqdda11@gmail.com>
2022-04-28 16:30:37 +0900

    Add Package & Mod customDecorator

--- src/commons/decorator/decorator.current.req.ts
+++ src/commons/decorator/decorator.current.req.ts
@@ -3,7 +3,7 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 export const CurrentRequest = createParamDecorator(
     (data: unknown, ctx: ExecutionContext) => {
         const request = ctx.switchToHttp().getRequest();
-        const findCheck = Object?.entries(request?.body)[0]
+        const findCheck = Object.entries(request?.body)[0]
 
         if (findCheck){
             return findCheck;
